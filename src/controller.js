1ff526af1e72cc7bd377d9def29d240ec0198b94
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2024-12-29 17:55:18 -0500

    Copy basic update() method from other file, try to modify.

--- src/controller.js
+++ src/controller.js
@@ -9,8 +9,88 @@ export default class SmithController extends LineController {
 	// Not needed since there is only a single scale
 	// eslint-disable-next-line class-methods-use-this, no-empty-function
 	linkScales() {}
+	update(mode) {
+		return;
+		const meta = this._cachedMeta;
+		const {dataset: line, data: points = [], _dataset} = meta;
+		// @ts-ignore
+		const animationsDisabled = this.chart._animationsDisabled;
+		let {start, count} = _getStartAndCountOfVisiblePoints(meta, points, animationsDisabled);
+
+		this._drawStart = start;
+		this._drawCount = count;
+
+		if (_scaleRangesChanged(meta)) {
+			start = 0;
+			count = points.length;
+		}
+
+		// Update Line
+		line._chart = this.chart;
+		line._datasetIndex = this.index;
+		line._decimated = !!_dataset._decimated;
+		line.points = points;
+
+		const options = this.resolveDatasetElementOptions(mode);
+		if (!this.options.showLine) {
+			options.borderWidth = 0;
+		}
+		options.segment = this.options.segment;
+		this.updateElement(line, undefined, {
+			animated: !animationsDisabled,
+			options
+		}, mode);
+
+		// Update Points
+		this.updateElements(points, start, count, mode);
+	}
 
 	updateElements(points, start, count, mode) {
+		const reset = mode === 'reset';
+		const {iScale, vScale, _stacked, _dataset} = this._cachedMeta;
+		const {sharedOptions, includeOptions} = this._getSharedOptions(start, mode);
+		const iAxis = iScale.axis;
+		const vAxis = vScale.axis;
+		const {spanGaps, segment} = this.options;
+		const maxGapLength = isNumber(spanGaps) ? spanGaps : Number.POSITIVE_INFINITY;
+		const directUpdate = this.chart._animationsDisabled || reset || mode === 'none';
+		const end = start + count;
+		const pointsCount = points.length;
+		let prevParsed = start > 0 && this.getParsed(start - 1);
+
+		for (let i = 0; i < pointsCount; ++i) {
+			const point = points[i];
+			const properties = directUpdate ? point : {};
+
+			if (i < start || i >= end) {
+				properties.skip = true;
+				continue;
+			}
+
+			const parsed = this.getParsed(i);
+			const nullData = isNullOrUndef(parsed[vAxis]);
+			const iPixel = properties[iAxis] = iScale.getPixelForValue(parsed[iAxis], i);
+			const vPixel = properties[vAxis] = reset || nullData ? vScale.getBasePixel() : vScale.getPixelForValue(_stacked ? this.applyStack(vScale, parsed, _stacked) : parsed[vAxis], i);
+
+			properties.skip = isNaN(iPixel) || isNaN(vPixel) || nullData;
+			properties.stop = i > 0 && (Math.abs(parsed[iAxis] - prevParsed[iAxis])) > maxGapLength;
+			if (segment) {
+				properties.parsed = parsed;
+				properties.raw = _dataset.data[i];
+			}
+
+			if (includeOptions) {
+				properties.options = sharedOptions || this.resolveDataElementOptions(i, point.active ? 'active' : mode);
+			}
+
+			if (!directUpdate) {
+				this.updateElement(point, i, properties, mode);
+			}
+
+			prevParsed = parsed;
+		}
+	}
+	updateElement_single(point, index, mode) {
 		const me = this;
 		const meta = me.getMeta();
 		const custom = point.custom || {};
@@ -30,7 +110,7 @@ export default class SmithController extends LineController {
 		point._index = index;
 
 		// Desired view properties
-		point._model = {
+		this.updateElement(point, index, {
 			x,
 			y,
 			skip: custom.skip || isNaN(x) || isNaN(y),
@@ -45,7 +125,7 @@ export default class SmithController extends LineController {
 			steppedLine: lineModel ? lineModel.steppedLine : false,
 			// Tooltip
 			hitRadius: options.hitRadius
-		};
+		}, mode);
 	}
 
 	/**
